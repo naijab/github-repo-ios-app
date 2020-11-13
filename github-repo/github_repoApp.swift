137e5eba3c5abeb037d6dc01a1e51b5b79f3095f
Nattapon Pondongnok <nainatjab999@gmail.com>
2020-11-14 02:38:14 +0700

    Initial Commit

---
+++ github-repo/github_repoApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_repoApp.swift
+//  github-repo
+//
+//  Created by Nattapon Pondongnok on 14/11/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct github_repoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
