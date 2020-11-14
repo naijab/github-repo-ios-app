5205f9104b779617b5312ae3fc074a5042cf6026
Nattapon Pondongnok <nainatjab999@gmail.com>
2020-11-14 07:11:17 +0700

    add github service

---
+++ github-repo/Services/GithubService.swift
@@ -0,0 +1,43 @@
+//
+//  GithubService.swift
+//  github-repo
+//
+//  Created by Nattapon Pondongnok on 14/11/2563 BE.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+class GithubService {
+    
+    private let baseUrl = "https://api.github.com"
+
+    func getRepoByUsername(_ username : String) -> Observable<[GithubRepository]> {
+        return Observable<[GithubRepository]>.create {observer in
+            AF.request("\(self.baseUrl)/users/\(username)/repos", method: .get, encoding: JSONEncoding.default, headers: nil)
+                .validate()
+                .response { response in
+                    print("=>> Fetch! Start")
+                    guard let responseData = response.data else {
+                        return observer.onCompleted()
+                    }
+                    
+                    do {
+                        let model = try JSONDecoder()
+                            .decode([GithubRepository].self,
+                                    from: responseData)
+                        print("=>> Fetch! Data : \(model.count)")
+                        observer.onNext(model)
+                        observer.onCompleted()
+                    } catch {
+                        observer.onError(error)
+                        print("=>> Featch! Error: \(error)")
+                        observer.onCompleted()
+                    }
+                }
+            return Disposables.create()
+        }
+    }
+    
+}
---
+++ github-repo/ViewModels/GithubViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  GithubViewModel.swift
+//  github-repo
+//
+//  Created by Nattapon Pondongnok on 14/11/2563 BE.
+//
+
+import Foundation
+
+class GithubViewModel: ObservableObject {
+    
+    @Published var repositories: [GithubRepository] = []
+    @Published var errorMessage: String = "Something Wrong"
+    @Published var isLoading: Bool = true
+    
+    func fetchGithubRepoByUsername(username: String) {
+        GithubService().getRepoByUsername(username).subscribe(
+            onNext: { data in
+                self.repositories = data
+                print("Data : \(data)")
+            },
+            onError: { error in
+                self.errorMessage = "Cannot Find This Username"
+                self.isLoading = false
+            },
+            onCompleted: {
+                self.isLoading = false
+            }
+        )
+    }
+    
+}
